f40c5501cebffdfe14d3d1ffd7a23ec668fb2d0b
Olga Bachishe <bachisheo@gmail.com>
2025-10-20 23:25:47 +0300

    update scripts

--- runners/scripts/collect_statistics.sh
+++ runners/scripts/collect_statistics.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 # run-progs.sh DIR TESTCASE
 # Example: ./collect_statistics.sh /home/olga/Documents/grigra/recs
-if [[ $# -lt 1 ]]; then
-  echo "Usage: $0 DIR" >&2
-  exit 1
-fi
+#if [[ $# -lt 1 ]]; then
+#  echo "Usage: $0 DIR" >&2
+#  exit 1
+#fi
+#dir="$1"
 
-dir="$1"
+cd ../..
+dataset="junit4-r4.12"
+path_to_data="/home/olga/src/dataset/grigra"
+dir="$path_to_data/mutated/$dataset"
 
 if [[ ! -d "$dir" ]]; then
   echo "Error: '$dir' is not a directory." >&2
@@ -16,6 +20,6 @@ fi
 for path in "$dir"/*; do
   [[ -f "$path" ]] || continue
   echo "run for $path"
-  ./gradlew runners:run --args="-i $path -o /home/olga/Documents/grigra/parsing_results"
+  ./gradlew runners:run --args="-i $path -o $path_to_data/parsing_results/$dataset"
 done
 
--- runners/scripts/mutate.sh
+++ runners/scripts/mutate.sh
@@ -1,1 +1,3 @@
-./gradlew :errecfuzz:app:run --args="-i /home/olga/src/dataset/clear/junit4-r4.12/ -o /home/olga/Documents/grigra/recs -d junit4-r4.12 -n 25 -r"
+dataset="RxJava-2.2.2"
+cd ../..
+./gradlew :errecfuzz:app:run --args="-i /home/olga/src/dataset/clear/$dataset -o /home/olga/src/dataset/grigra/mutated/$dataset -d $dataset -n 25 -r"
